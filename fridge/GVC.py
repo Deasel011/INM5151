dc8750606fe57a72d75531123632654be6c26c8a
Philippe Deslongchamps <philippe.deslongchamps@gmail.com>
2017-11-20 19:34:47 -0500

    Added Google Vision Communication class

--- fridge/GVC.py
+++ fridge/GVC.py
@@ -1,11 +1,19 @@
-import googleapiclient.sample_tools
+from google.cloud import storage
+
 import time
+
+from oauth2client.client import GOOGLE_APPLICATION_CREDENTIALS
 
 class GVC:
     def __init__(self):
-        connection_string = 'https://<YOUR_REGION>-<YOUR_PROJECT_ID>.cloudfunctions.net/helloHttp -H "Content-Type:application/json"' +' --data ' +'''{"name":"Keyboard Cat"}'''
+        client = storage.Client(credentials=GOOGLE_APPLICATION_CREDENTIALS)
+        bucket = client.get_bucket('image_facture')
+
 
     def sendPicture(self,userid,picture):
         atomicTimestamp = time.time()
-        data = {'data':picture
-                ,'name':userid+atomicTimestamp}
+        filename = userid + '_' + atomicTimestamp
+
+
+gvc = GVC()
+gvc.client.list_buckets()
