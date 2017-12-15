f8c49818a044be055bf35cc3888e78456073ce2f
shoul10 <mohammed_shl@hotmail.fr>
2017-12-14 23:15:06 -0500

    update produits

--- web/public/js/produits.js
+++ web/public/js/produits.js
@@ -1,26 +1,20 @@
 $(document).ready(function () {
-  var userid = 0;
-    $.ajax({
-        type: 'GET',
-        url: "http://inf5151-refrigerateur.appspot.com/inventaire/" + userid,
-        dataType: "json",
-        success: function (data) {
-          var listeBody = '';
-          $.each(data, function (i, item) {            
-            listeBody += '<li class="list-group-item">';
-            listeBody += '<img src="images/produits/'+ item.nom.replace(/ |'/gi, "_") +'.png" alt="" height="42" width="42">';
-            listeBody += '<b> '+ item.nom +'</b><span class="badge">'+ item.poids +'</span></li>';
-          });
-          $("#listeProduits").html(listeBody);
-              
-        },
-        error: function (err) {
-          alert('Erreur : ' + err.status);
-        },
-        failer: function () {
-          alert('La demande de ressource a échoué !');
-        }
-      });
 
-      
+
+  var userid = 0;
+  var proxy = 'https://cors-anywhere.herokuapp.com/';
+  $.ajax({
+    type: "GET",
+    url: proxy + "http://inf5151-refrigerateur.appspot.com/inventaire/" + userid
+  }).done(function (data) {
+    var listeBody = '';
+    $.each(data, function (i, item) {
+      listeBody += '<li class="list-group-item">';
+      listeBody += '<img src="images/produits/' + item.nom.replace(/ |'/gi, "_") + '.png" alt="" height="42" width="42">';
+      listeBody += '<b> ' + item.nom + '</b><span class="badge">' + item.quantite + '</span></li>';
+    });
+    $("#listeProduits").html(listeBody);
+  });
+
+
 });
