7707bb79a136691fa54024cf2257c0b6b8e66750
Philippe Deslongchamps <philippe.deslongchamps@gmail.com>
2017-12-15 20:37:23 -0500

    update view

--- fridge/views.sql
+++ fridge/views.sql
@@ -5,3 +5,11 @@ create or replace view v_inventaire (nom, quantite, code_usager) AS select p.nom
   join produitinventorie pi on pi.id_produit_inventorie = h.id_produit_inventorie
   join produit p on pi.id_produit = p.id_produit
   group by p.nom, h.code_usager;
+
+create or replace view v_recette_disponible (nom) as select r.nom
+from recette r
+join ingredient i on r.id_recette = i.id_recette
+join produitInventorie pi on  i.id_produit = pi.id_produit
+join h_produitinventorie hpi on pi.id_produit_inventorie = hpi.id_produit_inventorie
+group by hpi.code_usager
+where cast(sum(cast(left(hpi.quantite,length(hpi.quantite)-3) as int)) as varchar(15))|| ' ' || max(right(hpi.quantite,2)) >= i.quantite
